9d4045ac81d790f72c22ae3ae1cf20e32e2f60e6
Eric H <xpand.4beatz@gmail.com>
2018-09-10 21:39:46 +0200

    Added "devmarketer/easynav" and "laravelcollectiv/html"

--- config/app.php
+++ config/app.php
@@ -121,6 +121,11 @@ return [
 
     'providers' => [
 
+        /**
+         * Laravel Collevtive Providers...
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
         /*
          * Laravel Framework Service Providers...
          */
@@ -209,6 +214,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /**
+         * Laravel Collective
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
