3c5ffc32606937a2f868283d66b7917c026ff99d
xPand4B <xpand.4beatz@gmail.com>
2018-09-17 09:08:19 +0200

    Added route for imprint and privacy policy

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::view('/',            'pages.home')->name('home');
-Route::view('/contact',     'pages.contact')->name('contact');
-Route::view('/about',       'pages.about')->name('about');
+Route::view('/',                'pages.home')->name('home');
+Route::view('/contact',         'pages.contact')->name('contact');
+Route::view('/about',           'pages.about')->name('about');
 
+Route::view('/impressum',       'pages.impressum')->name('impressum');
+Route::view('/datenschutz',     'pages.datenschutz')->name('datenschutz');
 
-Route::redirect('/home',    '/', 301);
+Route::redirect('/home',        '/', 301);
