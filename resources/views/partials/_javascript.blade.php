267c9e2eab271b9818c050609717a51edf428974
Eric H <xpand.4beatz@gmail.com>
2018-10-31 21:26:17 +0100

    Changed javascript files path

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -1,14 +1,14 @@
 {{-- Core JS Files --}}
-{{ Html::script('js/vendor/jquery-3.3.1.min.js') }}
+{{ Html::script('js/jquery-3.3.1.min.js') }}
 {{ Html::script('js/bootstrap/popper.min.js') }}
 {{ Html::script('js/material_dashboard/bootstrap-material-design.min.js') }}
 {{ Html::script('js/material_dashboard/perfect-scrollbar.jquery.min.js') }}
 
 {{-- Chartist Plugin --}}
-{{ Html::script('js/material_dashboard/chartist.min.js') }}
+{{ Html::script('js/plugins/chartist.min.js') }}
 
 {{-- Notification Plugin --}}
-{{ Html::script('js/bootstrap/bootstrap-notify.js') }}
+{{ Html::script('js/plugins/bootstrap-notify.js') }}
 
 {{-- Core Center for Material Dashboard: parallax effects, scripts for the example pages etc. --}}
 {{ Html::script('js/material_dashboard/material-dashboard.min.js?v=2.1.0') }}
