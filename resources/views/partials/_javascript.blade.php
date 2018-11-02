13988294a24a71dba318ea29c3fab57b0f651e92
xPand4B <xpand.4beatz@gmail.com>
2018-11-02 09:53:43 +0100

    Included new main JS file

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -1,22 +1,3 @@
-{{-- Core JS Files --}}
-{{ Html::script('js/jquery-3.3.1.min.js') }}
-{{ Html::script('js/bootstrap/popper.min.js') }}
-{{ Html::script('js/material_dashboard/bootstrap-material-design.min.js') }}
-{{ Html::script('js/plugins/perfect-scrollbar.jquery.min.js') }}
-
-{{-- Chartist Plugin --}}
-{{ Html::script('js/plugins/chartist.min.js') }}
-
-{{-- Notification Plugin --}}
-{{ Html::script('js/plugins/bootstrap-notify.js') }}
-
-{{-- Core Center for Material Dashboard: parallax effects, scripts for the example pages etc. --}}
-{{ Html::script('js/material_dashboard/material-dashboard.min.js?v=2.1.0') }}
-
-{{-- Font-Awesome --}}
-{{ Html::script('js/plugins/fontawesome-all.min.js') }}
-
-{{-- Parsley-Validation --}}
-{{ Html::script('js/plugins/parsley.min.js') }}
+<script src="{{ asset('js/app.js') }}" defer></script>
 
 @yield('javascript')
