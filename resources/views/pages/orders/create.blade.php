47b803d881513a4ba09108e4ed80d45e9dd52611
xPand4B <xpand.4beatz@gmail.com>
2018-11-07 18:00:01 +0100

    Created "order.create" view

---
+++ resources/views/pages/orders/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', '| Bestellung erstellen')
+
+@section('headline')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb m-0 p-0 bg-transparent">
+            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-primary">Bestellungen</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Erstellen</li>
+        </ol>
+    </nav>
+@endsection
+
+@section('content')
+    
+@endsection
