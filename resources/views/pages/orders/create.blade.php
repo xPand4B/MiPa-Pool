48720f7ca12aa1fd0c2d371947468c8f02769918
xPand4B <eric.heinzl@gmail.com>
2018-11-13 08:54:51 +0100

    Changed breadcrumb color

--- resources/views/pages/orders/create.blade.php
+++ resources/views/pages/orders/create.blade.php
@@ -5,7 +5,7 @@
 @section('headline')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb m-0 p-0 bg-transparent">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-primary">Bestellungen</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-info">Bestellungen</a></li>
             <li class="breadcrumb-item active" aria-current="page">Erstellen</li>
         </ol>
     </nav>
