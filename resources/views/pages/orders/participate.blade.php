0fb2cacd288f09f99d9734f941a02d72d70fe47d
xPand4B <eric.heinzl@gmail.com>
2018-11-13 08:55:00 +0100

    Added page

---
+++ resources/views/pages/orders/participate.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', '| Mitbestellen')
+
+@section('headline')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb m-0 p-0 bg-transparent">
+            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-info">Bestellungen</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Bestellung bei Lieferheld</li>
+        </ol>
+    </nav>
+@endsection
+
+@section('content')
+    
+@endsection
