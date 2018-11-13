a740fe24bb17545b61fb06109612ecf333cfe393
xPand4B <eric.heinzl@gmail.com>
2018-11-13 08:53:25 +0100

    Changed background image

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -4,10 +4,11 @@
         @include('partials._head')
     </head>
     <body style="
-        background-image: url('https://source.unsplash.com/1280x720/weekly?tech');
+        background-image: url('https://source.unsplash.com/1280x720/daily?programming');
         background-repear: no-repeat;
         background-size: cover;
-        background-position: center center;">
+        background-position: center center;
+        background-repeat: no-repeat">
         
         <div class="wrapper">
             <div class="content mt-0 pt-3 pb-0">
