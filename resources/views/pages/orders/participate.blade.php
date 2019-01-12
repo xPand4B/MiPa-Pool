063e52649a5c91f6008cfbd56fdc17ef54b1f1c1
xPand4B <xpand.4beatz@gmail.com>
2019-01-12 19:29:35 +0100

    Added translations to order.participate view

--- resources/views/pages/orders/participate.blade.php
+++ resources/views/pages/orders/participate.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.main')
 
-@section('title', '| Mitbestellen')
+@section('title')
+    | @lang('page.order.participate.title')
+@endsection
 
 @section('headline')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb m-0 p-0 bg-transparent">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-info">Bestellungen</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Bestellung bei Lieferheld</li>
+            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-info">@lang('page.orders.breadcrumb.index')</a></li>
+            <li class="breadcrumb-item">@lang('page.orders.breadcrumb.participate')</li>
+            <li class="breadcrumb-item active" aria-current="page">{{ $order->name }}</li>
         </ol>
     </nav>
 @endsection
