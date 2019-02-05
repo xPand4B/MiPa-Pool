55747be8ee2f5ee3aa8ad89337a4fb950d2c464a
xPand4B <xpand.4beatz@gmail.com>
2019-02-05 19:11:30 +0100

    Created Menu Factory

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Menu::class, function (Faker $faker) {
+    return [
+        'menu'      => $faker->name(),
+        'comment'   => $faker->realText(rand(10, 50)),
+        'price'     => $faker->numberBetween(100, 2000)
+    ];
+});
