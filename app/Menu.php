9d41186ee1bb3c60ca5efd12187280f5a8a10d96
xPand4B <xpand.4beatz@gmail.com>
2019-02-05 19:09:18 +0100

    Created Menu model

---
+++ app/Menu.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    /**
+     * The attributes that are mass asignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'order_id', 'menu', 'comment', 'price'
+    ];
+
+    /**
+     * Get the user that added the menu.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the order for the created menu.
+     */
+    public function order()
+    {
+        return $this->belongsTo('App\Order');
+    }
+}
