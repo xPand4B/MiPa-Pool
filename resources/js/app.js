b234cc861235b569a3f400ca0cb872cb439f9275
xPand4B <xpand.4beatz@gmail.com>
2019-02-05 19:09:48 +0100

    Added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,16 +7,35 @@
 
 require('./bootstrap');
 
+window.Vue = require('vue');
+
 // Material Dashboard
     // Core JS Files
     require('../../node_modules/material-dashboard/assets/js/core/jquery.min');
     require('../../node_modules/material-dashboard/assets/js/core/popper.min');
     require('../../node_modules/material-dashboard/assets/js/core/bootstrap-material-design.min');
+
     // Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/
     require('../../node_modules/material-dashboard/assets/js/plugins/bootstrap-notify');
+
     // Charts Plugin, full documentation here: https://gionkunz.github.io/chartist-js/
     require('../../node_modules/material-dashboard/assets/js/plugins/chartist.min.js');
+
     // Plugin for Scrollbar documentation here: https://github.com/utatti/perfect-scrollbar
     require('../../node_modules/material-dashboard/assets/js/plugins/perfect-scrollbar.jquery.min.js');
+
     // Material Dashboard Core initialisations of plugins and Bootstrap Material Design Library
     require('../../node_modules/material-dashboard/assets/js/material-dashboard.js?v=2.1.0');
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+// const app = new Vue({
+//     el: '#app'
+// });
