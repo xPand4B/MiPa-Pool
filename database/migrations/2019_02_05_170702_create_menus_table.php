678ed874a0b936f42dcd4c757a377145af06ed44
xPand4B <xpand.4beatz@gmail.com>
2019-02-05 19:09:31 +0100

    Created Menu migration

---
+++ database/migrations/2019_02_05_170702_create_menus_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('order_id')->unsigned();
+            $table->string('menu', 100);
+            $table->text('comment');
+            // Instead of 5,65€ + 6,33€ this value is multiplied by 10^-2
+            // 5,65 + 6,33 = 14,98
+            // (565 + 633) = 1498
+            // 1498 * 10^-2 = 14,98
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
