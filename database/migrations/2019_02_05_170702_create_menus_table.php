218a2ac661f302aaff9a0160c64ac2f7de56488b
xPand4B <xpand.4beatz@gmail.com>
2019-02-09 11:46:32 +0100

    Changed comment

--- database/migrations/2019_02_05_170702_create_menus_table.php
+++ database/migrations/2019_02_05_170702_create_menus_table.php
@@ -19,7 +19,7 @@ class CreateMenusTable extends Migration
             $table->bigInteger('order_id')->unsigned();
             $table->string('menu', 100);
             $table->text('comment');
-            // Instead of 5,65€ + 6,33€ this value is multiplied by 10^-2
+            // Instead of 5,65€ + 6,33€ this value is multiplied by 10^-2 or 0,01
             // 5,65 + 6,33 = 14,98
             // (565 + 633) = 1498
             // 1498 * 10^-2 = 14,98
