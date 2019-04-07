a8efc87af09cf6534dddc4031f874e5307cf4132
xPand4B <xpand.4beatz@gmail.com>
2019-04-07 13:52:53 +0200

    Changed Spend format + added avatar validation

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Menu;
@@ -34,17 +35,27 @@ class ProfileController extends Controller
         // 22670
         $money_spend = Menu::where('user_id', '=', $user->id)
                             ->sum('price');
-        $money_spend = $money_spend * 0.01;
+        $money_spend *= 0.01;
+
+        if($money_spend != '0'){
+            if(strlen($money_spend) != 1){
+                
+                if($money_spend < 10){
+                    $money_spend = '0'.$money_spend;
+                }
+                
+                if(strlen($money_spend) == 4){
+                    $money_spend = $money_spend.'0';
+                }
+            }
+        }
+
 
         $order_count = Auth::user()->orders->count();
 
         $this_month = Order::where('user_id', '=', $user->id)
                             ->whereMonth('created_at', date('m'))
                             ->count();
-
-        // echo '<br>' . $this_month;
-
-        // exit();
 
         return view('pages.profile', [
             'user' => $user,
@@ -64,6 +75,23 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $user = Auth::user();
+
+        // Check if avatar has changed
+        if($request->hasFile('avatar')){
+            $request->validate([
+                'avatar' => 'image|mimes:jpeg,jpg,png,gif,svg|max:2048'
+            ]);
+
+            if($user->avatar != 'user.svg'){
+                Storage::delete('avatars/'.$user->avatar);
+            }
+
+            $avatarName = $user->id.'_avatar'.time().'.'.request()->avatar->getClientOriginalExtension();
+
+            $request->avatar->storeAs('avatars',$avatarName);
+
+            $user->avatar = $avatarName;
+        }
 
         // Check if username has changed
         if($request->input('username') != $user->username){
