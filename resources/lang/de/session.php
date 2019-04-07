a58f640a2c6980d0e897cfb7d43928030d5cdac9
xPand4B <xpand.4beatz@gmail.com>
2019-04-07 14:41:25 +0200

    Created  session translation

---
+++ resources/lang/de/session.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Session Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain all session specific translations.
+    |
+    */
+
+    'title' => [
+        'success'   => 'Erfolgreich:',
+        'error'     => 'Fehler:',
+    ],
+
+    'order' =>[
+        'created' => 'Deine Bestellung wurde erstellt.',
+    ],
+    
+    'profile' => [
+        'updated' => 'Dein Profil wurde aktualisiert.',
+    ],
+];
---
+++ resources/lang/en/session.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Session Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain all session specific translations.
+    |
+    */
+
+    'title' => [
+        'success'   => 'Success:',
+        'error'     => 'Error:'
+    ],
+
+    'order' =>[
+        'created' => 'Your order has been created.',
+    ],
+    
+    'profile' => [
+        'updated' => 'Your profile has been updated.',
+    ],
+
+];
