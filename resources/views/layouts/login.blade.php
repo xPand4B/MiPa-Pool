269531112f7b97a20a4c38cb808ff23e592253b7
xPand4B <xpand.4beatz@gmail.com>
2019-05-25 08:03:56 +0200

    FIXED: Floating labels now available in login view

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -10,6 +10,8 @@
         background-position: center center;
         background-repeat: no-repeat">
         
+        <script src="{{ asset('js/app.js') }}"></script>
+        
         <div class="wrapper">
             <div class="content mt-0 pt-3 pb-0">
                 <div class="container-fluid">
