144df31600d6b881a81b1c7642aaea0de0937a3d
xPand4B <xpand.4beatz@gmail.com>
2019-05-25 08:30:31 +0200

    Added 'locale' to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->text('aboutMe')->nullable();
             $table->string('avatar')->default('user.svg');
+            $table->string('locale')->default('en');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
