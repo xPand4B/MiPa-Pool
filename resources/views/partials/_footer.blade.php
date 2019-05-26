f75b68f48cad6724ebee49396e76f74f36c78e19
xPand4B <xpand.4beatz@gmail.com>
2019-05-26 21:04:51 +0200

    Removed footer links

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -10,11 +10,8 @@
         {{-- Right Sided Items --}}
         <nav class="float-right text-info">
             <ul>
-                <li><a href="{{ route('imprint') }}">
-                    @lang('menu.footer.imprint')</a></li>
-
-                <li><a href="{{ route('privacy_policy') }}">
-                    @lang('menu.footer.privacy_policy')</a></li>
+                {{-- <li><a href="#">
+                    @lang('')</a></li> --}}
             </ul>
         </nav>
     </div>
