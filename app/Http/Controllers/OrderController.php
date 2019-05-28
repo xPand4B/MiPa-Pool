4e5aea0d50e1a538d8102b146252b5c654f66cc3
xPand4B <xpand.4beatz@gmail.com>
2019-05-29 00:13:28 +0200

    Added participate functions + sum calc

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Order;
+use App\Menu;
 use Session;
 
 class OrderController extends Controller
@@ -27,22 +28,36 @@ class OrderController extends Controller
         $orders = Order::orderBy('id', 'desc')->paginate(15);
 
         for($i = 0; $i < sizeof($orders); $i++){
+            
+            $sum = 0;
+
             for($j = 0; $j < sizeof($orders[$i]->menus); $j++){
-                $price = $orders[$i]->menus[$j]->price;
-
-                $price *= 0.01;
-
+                $price = $orders[$i]->menus[$j]->price * 0.01;
+                $sum  += $price;
+    
                 if($price < 10)
                     $price = '0'.$price;
                 
                 if(strlen($price) == 4)
                     $price = $price.'0';
-
+    
                 if(strlen($price) == 2)
                     $price = $price.'.00';
-
+    
                 $orders[$i]->menus[$j]->price = $price;
             }
+
+            if($sum < 10)
+                $sum = '0'.$sum;
+            
+            if(strlen($sum) == 4){
+                $sum = $sum.'0';
+
+            }else if(strlen($sum) == 2){
+                $sum = $sum.'.00';
+            }
+
+            $orders[$i]->sum = str_replace('.', ',', $sum);
         }
         
         return view('pages.orders.index', [
@@ -82,14 +97,14 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        // $request->validate([
-        //     'order_name'            => 'required|min:5|max:128',
-        //     'deadline'              => 'required',
-        //     'max_orders'            => 'required|integer|between:2,20',
-        //     'minimum_order_value'   => 'required|integer|between:0,20',
-        //     'delivery_service'      => 'required|string',
-        //     'site_link'             => 'required|active_url'
-        // ]);
+        $request->validate([
+            'order_name'            => 'required|min:5|max:128',
+            'deadline'              => 'required',
+            'max_orders'            => 'required|integer|between:2,20',
+            'minimum_order_value'   => 'required|integer|between:0,20',
+            'delivery_service'      => 'required|string',
+            'site_link'             => 'required|active_url'
+        ]);
 
         $order = new Order;
             $order->user_id     = $request->user()->id;
@@ -111,15 +126,87 @@ class OrderController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function participate($id)
     {
-        $order = Order::find($id);
+        if(!ctype_digit($id))
+            return redirect()->back();
 
+        $order = Order::find($id);
+        $sum   = 0;
+
+        for($j = 0; $j < sizeof($order->menus); $j++){
+            
+            $price = $order->menus[$j]->price * 0.01;
+            $sum  += $price;
+
+            if($price < 10)
+                $price = '0'.$price;
+            
+            if(strlen($price) == 4)
+                $price = $price.'0';
+
+            if(strlen($price) == 2)
+                $price = $price.'.00';
+
+            $order->menus[$j]->price = $price;
+        }
+
+        if($sum < 10)
+            $sum = '0'.$sum;
+        
+        if(strlen($sum) == 4){
+            $sum = $sum.'0';
+
+        }else if(strlen($sum) == 2){
+            $sum = $sum.'.00';
+        }
+        
         return view('pages.orders.participate', [
-            'order' => $order
+            'order' => $order,
+            'sum'   => str_replace('.', ',', $sum)
         ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function storeParticipate(Request $request)
+    {
+        $request->validate([
+            'name'      => 'required|min:5|max:128',
+            'number'    => 'required|between:1,10',
+            'price'     => 'required|min:0|max:6',
+            'comment'   => 'max:255'
+        ]);
+
+        $price = str_replace(',', '.', $request->price);
+
+        if(!is_numeric($price)){
+            Session::flash('error', trans('session.order.price_is_no_digit'));
+            return redirect()->back();
+        }
+
+        $menu = new Menu;
+            $menu->user_id  = $request->user()->id;
+            $menu->order_id = $request->order_id;
+            $menu->menu     = $request->name;
+            $menu->number   = $request->number;
+            $menu->comment  = $request->comment;
+            $menu->price    = $price * 100;
+            // $menu->price = 10;
+
+            $menu->save();
+
+        Session::flash('success', trans('session.order.participated'));
+
+        return redirect()->route('home');
     }
 
     /**
