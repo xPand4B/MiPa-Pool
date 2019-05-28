d863d4bdc3a7308346e311b7e7b525988ff96306
xPand4B <xpand.4beatz@gmail.com>
2019-05-29 00:08:47 +0200

    Added manual error session message

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -22,7 +22,7 @@
                         '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
 
                         '<span data-notify="title">' +
-                            '<i class="fa fa-lg fa-check-circle-o" aria-hidden="true" style="color:#fff"></i> ' +
+                            '<i class="fa fa-lg fa-check-circle-o" aria-hidden="true" style="color:#fff"></i> &ensp;' +
                             '{1}' +
                         '</span>' +
 
@@ -60,7 +60,7 @@
                         '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
 
                         '<span data-notify="title">' +
-                            '<i class="fa fa-lg fa-info-circle" aria-hidden="true" style="color:#fff"></i> ' +
+                            '<i class="fa fa-lg fa-info-circle" aria-hidden="true" style="color:#fff"></i> &ensp;' +
                             '{1}' +
                         '</span> ' +
 
@@ -75,6 +75,44 @@
                     '</div>' 
        });
    </script>
+
+   @elseif(Session::has('error') || $message = Session::get('error'))
+   <script>
+       $.notify({
+           title:      '{{ trans("session.title.error") }}',
+           message:    '{{ Session::get("error") }}',
+      },{
+          type:            'danger',
+          delay:           2000,
+          allow_dismiss:   true,
+          showProgressbar: false,
+          animate: {
+              enter:  'animated fadeInDown',
+              exit:   'animated fadeOutUp'
+          },
+          placement: {
+              from:   'top',
+              align:  'center'
+          },
+          template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                       '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+
+                       '<span data-notify="title">' +
+                           '<i class="fa fa-lg fa-exclamation-triangle" aria-hidden="true" style="color:#fff"></i> &ensp;' +  
+                           '{1}' +
+                       '</span> ' +
+
+                       '<span data-notify="message">' +
+                           '<h4>{2}</h4>' +
+                       '</span>' +
+
+                       '<div class="progress mt-2" data-notify="progressbar">' +
+                           '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                       '</div>' +
+                       '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                   '</div>' 
+      });
+  </script>
 @endif
 
 @if (count($errors) > 0)
@@ -99,7 +137,7 @@
                         '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
 
                         '<span data-notify="title">' + 
-                            '<i class="fa fa-lg fa-exclamation-triangle" aria-hidden="true" style="color:#fff"></i>' +
+                            '<i class="fa fa-lg fa-exclamation-triangle" aria-hidden="true" style="color:#fff"></i> &ensp;' +
                             '{1}' +
                         '</span> ' +
 
