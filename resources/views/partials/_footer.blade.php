22676de5d5de9063366a80c455b868075052a729
xPand4B <xpand.4beatz@gmail.com>
2019-05-29 00:09:02 +0200

    Changed footer styling

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer" style="border-top: none">
     <div class="container-fluid">
         {{-- Left Sided Items --}}
         <nav class="float-left text-dark">
@@ -8,10 +8,10 @@
         </nav>
 
         {{-- Right Sided Items --}}
-        <nav class="float-right text-info">
+        <nav class="float-right text-dark">
             <ul>
                 {{-- Source Code --}}
-                <li class="text-dark">
+                <li>
                     <a href="https://github.com/xPand4B/MiPa-Pool" target="_blank">
                         <i class="fa fa-lg fa-github"></i> &ensp; @lang('menu.footer.sourcecode')
                     </a>
