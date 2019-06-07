aee953ff697560876eee26edf505ccc2b26de1ab
xPand4B <xpand.4beatz@gmail.com>
2019-06-07 15:00:22 +0200

    Moved js partial to the top

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,13 +4,13 @@
         @include('partials._head')
     </head>
     <body>
-        <div class="wrapper">
-            
+        @include('partials._javascript')
+        
+        <div class="wrapper">    
             @include('partials._sidenav')
 
             {{-- https://webgradients.com/ --}}
             <div class="main-panel" style="background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);">
-            {{-- <div class="main-panel" style="background-color: #E4E4E1;background-image: radial-gradient(at top center, rgba(255,255,255,0.03) 0%, rgba(0,0,0,0.03) 100%), linear-gradient(to top, rgba(255,255,255,0.1) 0%, rgba(143,152,157,0.60) 100%);background-blend-mode: normal, multiply;"> --}}
                 @include('partials._topnav')
                 @include('partials._messages')
 
@@ -25,7 +25,5 @@
                 @include('partials._footer')
             </div>
         </div>
-        
-        @include('partials._javascript')
     </body>
 </html>
