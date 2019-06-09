1954f61c24ab24f1383d0d62c7a124f12729798c
xPand4B <xpand.4beatz@gmail.com>
2019-06-09 19:30:29 +0200

    Added custom email for sending verify link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,34 @@
 
 namespace App\Providers;
 
+use Auth;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
      *
-     * @return void
      */
     public function boot()
     {
-        //
+        VerifyEmail::toMailUsing(function($notifiable) {
+            $verifyURL = URL::temporarySignedRoute(
+                'verification.verify', Carbon::now()->addMinute(60), ['id' => $notifiable->getKey()]
+            );
+
+            return (new MailMessage())
+                ->subject(trans('mail.verify.subject'))
+                ->line(trans('mail.verify.line'))
+                ->action(trans('mail.verify.action'), $verifyURL)
+                ->markdown('mails.verify', [
+                    'user' => Auth::user(),
+                ]);
+        });
     }
 
     /**
