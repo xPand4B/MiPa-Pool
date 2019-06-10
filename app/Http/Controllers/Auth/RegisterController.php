4fd0e6333f0b4815844682c6f879466e43acf268
xPand4B <xpand.4beatz@gmail.com>
2019-06-10 10:46:41 +0200

    Revert "Revert "Revert "Commented out everything email related issue#03"""
    This reverts commit 99dd8f0fe9598ea36cfce990b7b82847c89142fc.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
             'username'  => 'required|string|max:255|unique:users',
             'firstname' => 'required|string|max:255',
             'surname'   => 'required|string|max:255',
-            // 'email'     => 'required|string|email|max:255|unique:users',
+            'email'     => 'required|string|email|max:255|unique:users',
             'password'  => 'required|string|min:6|confirmed',
         ]);
     }
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'username'  => $data['username'],
             'firstname' => $data['firstname'],
             'surname'   => $data['surname'],
-            // 'email'     => $data['email'],
+            'email'     => $data['email'],
             'password'  => Hash::make($data['password']),
         ]);
     }
