e4ee1b19b6391dea5745dc2ff66597d5a63672aa
xPand4B <xpand.4beatz@gmail.com>
2019-06-12 17:10:36 +0200

    Added SearchController

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Searchable\Search;
+use App\Order;
+use App\Menu;
+use App\User;
+
+class SearchController extends Controller
+{
+    /**
+     * Restrict access
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        $search = request('q');
+
+        if(! isset($search))
+            return redirect()->route('home');
+
+        $users  = User::searchFor($search)->paginate(20);
+
+        $menus  = Menu::searchFor($search)->paginate(20);
+
+        $orders = Order::search($search)
+                        ->NotClosed()
+                        ->paginate(15);
+
+        return view('pages.search.show', compact([
+            'search', 'users', 'menus', 'orders'
+        ]));
+    }
+}
