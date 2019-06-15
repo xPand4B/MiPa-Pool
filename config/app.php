4e16d37cb9ffbae11cca2176fab56484294385b6
xPand4B <xpand.4beatz@gmail.com>
2019-06-15 09:32:55 +0200

    Added barryvdh/laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -214,11 +214,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        /**
-         * Laravel Collective
+        /*
+         * Package Aliases...
          */
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
         /**
          * Custom aliases
