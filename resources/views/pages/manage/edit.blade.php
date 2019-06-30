2e96622bb1a21eb80daf96975e70dd55689faa36
Eric H <xpand.4beatz@gmail.com>
2019-06-30 13:30:26 +0200

    Created manage show and edit views

---
+++ resources/views/pages/manage/edit.blade.php
@@ -0,0 +1,138 @@
+@foreach ($orders as $order)
+    @if (! $order->closed)
+        <div class="modal fade" id="orders.edit.{{ $order->id }}" tabindex="-1" role="dialog" aria-labelledby="{{ $order->name }}" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    
+                    {{-- Header --}}
+                    <div class="modal-header">
+                        {{-- <h5 class="modal-title">Modal title</h5> --}}
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    {{-- Content --}}
+                    <div class="modal-body p-0">
+                        {{-- Card Header --}}
+                        <div class="card-header card-header-transparent card-header-icon">
+                            <div class="card-icon p-0 m-0 bg-transparent row text-center" style="float: none">
+                                
+                                {{-- Lieferheld --}}
+                                <div class="col">
+                                    <img src="{{ asset('img/lieferheld_400x400.png') }}" title="" width="82px" height="82px" style="border-radius: 41px" data-toggle="modal" data-target="#mysticModal01">
+                                </div>
+
+                                {{-- Pizza.de --}}
+                                <div class="col">
+                                    <img src="{{ asset('img/pizza.de_1200x1200.png') }}" title="" width="82px" height="82px" style="border-radius: 41px" data-toggle="modal" data-target="#mysticModal01">
+                                </div>
+
+                                {{-- Lieferando --}}
+                                <div class="col">
+                                    <img src="{{ asset('img/lieferando_512x512.png') }}" title="" width="82px" height="82px" style="border-radius: 41px" data-toggle="modal" data-target="#mysticModal01">
+                                </div>
+                            </div>
+                        </div>
+                        
+                        {{-- Card Content --}}
+                        <div class="card-body">
+                            {!! Form::model($order, [ 'route'  => [ 'orders.update', $order ], 'method' => 'PUT' ]) !!}
+                                @csrf
+                                {{-- Order Name --}}
+                                <div class="form-group is-focused">
+                                    {{ Form::label('name', trans('page.manage.edit.form.order_name'), ['class' => 'bmd-label-floating']) }}
+                
+                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ $order->name }}" required autofocus>
+                
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                {{-- Deadline --}}
+                                <div class="form-group col pl-0 pr-3">
+                                    {{ Form::label('deadline', trans('page.manage.edit.form.deadline'), ['class' => 'bmd-label-floating']) }}
+
+                                    <select name="deadline" id="deadline" class="form-control text-gray" disabled>
+                                        <option value="" selected>{{ date('d.m.Y - H:i', strtotime($order->deadline)) }} @lang('page.manage.edit.form.time')</option>
+                                    </select>
+                                </div>
+                
+                                {{-- Max Orders | Minimum order value --}}
+                                <div class="form-row" style="padding-left: 5px">
+
+                                    {{-- Max Orders --}}
+                                    <div class="form-group col px-0 pr-3">
+                                        {{ Form::label('max_orders', trans('page.manage.edit.form.max_orders'), ['class' => 'bmd-label-floating']) }}
+
+                                        <select name="max_orders" id="max_orders" class="form-control" required>
+                                            @for ($i = 2; $i <= 20; $i++)
+                                                @if ($i == $order->max_orders)
+                                                    <option value="{{ $i }}" selected>{{ $i }}</option>
+                                                @else
+                                                    <option value="{{ $i }}">{{ $i }}</option>
+                                                @endif
+                                            @endfor
+                                        </select>
+                                    </div>
+
+                                    {{-- Minimun order value --}}
+                                    <div class="form-group col">
+                                        {{ Form::label('minimum_value', trans('page.manage.edit.form.minimum_order_value'), ['class' => 'bmd-label-floating']) }}
+
+                                        <select name="minimum_value" id="minimum_value" class="form-control" required>
+                                            @for ($i = 0; $i <= 20; $i++)
+                                                @if ($i == $order->minimum_value)
+                                                    <option value="{{ $i }}" selected>{{ $i }} {{ config('app.currency') }}</option>
+                                                @else
+                                                    <option value="{{ $i }}">{{ $i }} {{ config('app.currency') }}</option>
+                                                @endif
+                                            @endfor
+                                        </select>
+                                    </div>
+                                </div>
+                
+                                {{-- Delivery Service --}}
+                                <div class="form-group col pl-0 pr-3">
+                                    {{ Form::label('delivery_service', trans('page.manage.edit.form.delivery_service'), ['class' => 'bmd-label-floating']) }}
+            
+                                    <input id="delivery_service" type="text" class="form-control{{ $errors->has('delivery_service') ? ' is-invalid' : '' }}" name="delivery_service" value="{{ $order->delivery_service }}" required>
+            
+                                    @if ($errors->has('delivery_service'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('delivery_service') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                
+                                {{-- Site Link --}}
+                                <div class="form-group col px-0">
+                                    {{ Form::label('site_link', trans('page.manage.edit.form.site_link'), ['class' => 'bmd-label-floating']) }}
+            
+                                    <input id="site_link" type="text" class="form-control{{ $errors->has('site_link') ? ' is-invalid' : '' }}" name="site_link" value="{{ $order->site_link }}" required>
+            
+                                    @if ($errors->has('site_link'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('site_link') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                {{-- Submit --}}
+                                <div class="form-group row">
+                                    <button type="submit" class="btn btn-block btn-success btn-round">
+                                        {!! config('icons.shopping-cart') !!} &ensp; @lang('page.manage.edit.form.submit')
+                                    </button>
+                                </div>
+                            {{ Form::close() }}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    @endif
+@endforeach
--- resources/views/pages/manage/show.blade.php
+++ resources/views/pages/manage/show.blade.php
@@ -1,16 +1,135 @@
-@extends('layouts.main')
+@foreach ($orders as $order)
+    <div class="modal fade" id="orders.show.{{ $order->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
 
-@section('title', trans('page.manage.title'))
+                {{-- Header --}}
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
 
-@section('headline')
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb m-0 p-0 bg-transparent">
-            <li class="breadcrumb-item"><a href="{{ route('manage.index') }}" class="text-info">@lang('page.manage.breadcrumb.index')</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{ $order->name }}</li>
-        </ol>
-    </nav>
-@endsection
+                {{-- Content --}}
+                <div class="modal-body p-0">
 
-@section('content')
-    
-@endsection
+                    {{-- Card Header --}}
+                    <div class="card-header card-header-icon">
+                        {{-- User Avatar --}}
+                        <div class="card-icon p-0 bg-transparent">
+                            @if (file_exists(realpath(config('filesystems.avatar.path').$order->user->avatar)))
+                                <img 
+                                    class="rounded-circle"
+                                    src="{{ asset(config('filesystems.avatar.path').$order->user->avatar) }}"
+                                    title="{{ $order->user->firstname }} {{ $order->user->surname}} ({{ $order->user->username }})" 
+                                    width="64px"
+                                    height="64px">
+                            @endif
+                        </div>
+
+                        {{-- Title --}}
+                        <h4 class="card-title row mt-0">
+                            <strong class="col-md-8 mt-4">
+                                <div class="text-dark">
+                                    {{ $order->name }}
+                                </div>
+
+                                <small>
+                                    <u>
+                                        <a href="{{ $order->site_link }}" target="_blank" class="text-muted">
+                                            {{ $order->delivery_service }}
+                                        </a>
+                                    </u>
+                                </small>
+                            </strong>
+                        </h4>
+                    </div>
+
+                    {{-- Card Content --}}
+                    <div class="card-content mt-2">
+                        <div class="container-fluid p-0">
+                            @if ($order->menus->isNotEmpty())
+                                <table class="table table-sm table-shopping text-center m-0">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th class="p-1">@lang('table.orders.head.name')</th>
+                                            <th class="p-1">@lang('table.orders.head.menu')</th>
+                                            <th class="p-1">@lang('table.orders.head.number')</th>
+                                            <th class="p-1">@lang('table.orders.head.comment')</th>
+                                            <th class="p-1">@lang('table.orders.head.price')</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($order->menus as $menu)
+                                            @if (Auth::user()->id == $menu->user_id)
+                                            <tr class="text-primary">
+                                            @else
+                                            <tr>
+                                            @endif
+                                                <td class="p-1">{{ $menu->user->firstname }} {{ $menu->user->surname }}</td>
+                                                <td class="p-1">{{ $menu->name }}</td>
+                                                <td class="p-1">{{ $menu->number }}</td>
+                                                <td class="p-1">{{ $menu->comment }}</td>
+                                                <td class="p-1">{{ $menu->price }} {{ config('app.currency') }}</td>
+                                            </tr>
+                                        @endforeach
+                                        
+                                        <tr>
+                                            <td class="p-1"></td>
+                                            <td class="p-1"></td>
+                                            <td class="p-1"></td>
+                                            <td class="p-1"></td>
+                                            <th class="p-1 text-center"><strong>{{ $order->sum }} {{ config('app.currency') }}</strong></th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                
+                            @else
+                                <div class="text-center">
+                                    <h5 class="text-danger"><strong>@lang('table.orders.empty')</strong></h4>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+
+                    <hr class="mt-2 mb-0">
+
+                    {{-- Card Footer --}}
+                    <div class="card-footer">
+                        {{-- Deadline --}}
+                        <div class="col-md text-center">
+                            @if ($order->timeLeft_min <= 5 || $order->closed)
+                            <div class="stats text-danger">
+
+                            @elseif($order->timeLeft_min <= 10)
+                            <div class="stats text-warning">
+
+                            @else
+                            <div class="stats text-success">
+                            @endif
+
+                                {!! config('icons.time') !!} {{ $order->closed ? trans('page.manage.show.noTimeLeft') : $order->timeLeft }}
+                            </div>
+                        </div>
+
+                        {{-- Participants --}}
+                        <div class="col-md text-center">
+                            @if (sizeof($order->menus) == $order->max_orders)
+                                <div class="stats text-danger">
+                            @elseif(sizeof($order->menus) != 0 && (sizeof($order->menus) + 1 == $order->max_orders || sizeof($order->menus) + 2 == $order->max_orders))
+                                <div class="stats text-warning">
+                            @else
+                                <div class="stats text-success">
+                            @endif
+
+                                {!! config('icons.checked') !!} {{ sizeof($order->menus) }}/{{ $order->max_orders }} @lang('table.orders.footer.people_count')
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
