3b5f03a5a01e5652059a5e13ec3600bf91cc6287
Yunarito <Yunarito@users.noreply.github.com>
2020-03-25 15:56:26 +0100

    NTR - Created Authentication Design

--- resources/js/src/pages/auth/Login.vue
+++ resources/js/src/pages/auth/Login.vue
@@ -1,10 +1,91 @@
 <template>
     <div>
-        login
+        <v-form
+            @submit.prevent="submit"
+            v-model="valid"
+        >
+            <v-text-field
+                id="email"
+                label="E-Mail"
+                name="email"
+                v-model="form.email"
+                :rules="[rules.email, rules.length, rules.required]"
+                prepend-icon="far fa-envelope"
+                type="email"
+                selected
+                required
+            />
+
+            <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                v-model="form.password"
+                :rules="[rules.length, rules.required]"
+                prepend-icon="fas fa-key"
+                :append-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                @click:append="passwordVisible = !passwordVisible"
+                :type="passwordVisible ? 'text' : 'password'"
+                required
+            />
+            
+            <v-checkbox
+                v-model="form.remember"
+                label="Remember me?"
+            />
+
+            <v-divider class="py-3"/>
+
+            <v-btn
+                :disabled="!valid"
+                color="green"
+                type="submit"
+                rounded
+                block
+                large
+            >
+                <icon name="fas fa-sign-in-alt"/> &nbsp; Login
+            </v-btn>
+        </v-form>
     </div>
 </template>
 
 <script>
     export default {
+        name: "auth-login",
+
+        data: () => ({
+            valid: false,
+            form: {
+                email: '',
+                password: '',
+                remember: false
+            },
+            passwordVisible: false,
+            rules: {
+                required: value => !!value || 'Field is required',
+                email: value => {
+                    const pattern = /.+@.+\..+/;
+                    return (
+                        pattern.test(value) ||
+                        'E-Mail must be valid.'
+                    );
+                },
+                length: value => {
+                    const minLength = 5;
+                    const maxLength = 255;
+                    return (
+                        (value.length >= minLength && value.length <= maxLength) ||
+                        'Field must be between 5 and 255 characters'
+                    );
+                }
+            },
+        }),
+
+        methods: {
+            submit() {
+                console.log('Submit')
+            },
+        },
     }
 </script>
