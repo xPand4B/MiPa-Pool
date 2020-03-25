ad388076d21e3c6e1bd6c146f19c0c474398e7ae
Yunarito <Yunarito@users.noreply.github.com>
2020-03-25 15:56:26 +0100

    NTR - Created Authentication Design

--- src/Frontend/Resources/app/modules/layout/page/layout-auth/index.vue
+++ src/Frontend/Resources/app/modules/layout/page/layout-auth/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-content>
+    <v-content class="authBackground">
         <v-container
             class="fill-height"
             fluid
@@ -13,57 +13,37 @@
                     sm="8"
                     md="4"
                 >
-                    <v-card class="elevation-12">
+                    <v-card class="elevation-12" shaped>
                         <v-toolbar
                             color="primary"
                             dark
                             flat
                         >
-                            <v-toolbar-title>Login</v-toolbar-title>
+                            <v-tabs
+                                v-model="tab"
+                                fixed-tabs
+                                dark
+                            >
+                                <v-tab>
+                                    Login
+                                </v-tab>
+                                <v-tab>
+                                    Register
+                                </v-tab>
+                            </v-tabs>
                             <v-spacer/>
                         </v-toolbar>
+
                         <v-card-text>
-                             <v-form
-                                 @submit.prevent="submit"
-                                 v-model="valid"
-                             >
-                                 <v-text-field
-                                     id="email"
-                                     label="E-Mail"
-                                     name="email"
-                                     v-model="form.email"
-                                     :rules="[rules.email, rules.length, rules.required]"
-                                     type="email"
-                                     selected
-                                     required
-                                 />
+                            <v-tabs-items v-model="tab">
+                                <v-tab-item>
+                                    <auth-login/>
+                                </v-tab-item>
+                                <v-tab-item>
+                                    <auth-register/>
+                                </v-tab-item>
 
-                                 <v-text-field
-                                     id="password"
-                                     label="Password"
-                                     name="password"
-                                     v-model="form.password"
-                                     :rules="[rules.length, rules.required]"
-                                     :append-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-                                     @click:append="passwordVisible = !passwordVisible"
-                                     :type="passwordVisible ? 'text' : 'password'"
-                                     required
-                                 />
-
-                                 <v-checkbox
-                                     v-model="form.remember"
-                                     label="Remember me?"
-                                 />
-                            </v-form>
-
-                            <v-btn
-                                :disabled="!valid"
-                                color="green"
-                                type="submit"
-                                rounded
-                            >
-                                <icon name="fas fa-sign-in-alt"/> &nbsp; Login
-                            </v-btn>
+                            </v-tabs-items>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -73,45 +53,34 @@
 </template>
 
 <script>
-    export default {
-        name: 'layout-page-auth',
+    import AuthLogin from "../../pages/auth/Login";
+    import AuthRegister from "../../pages/auth/Register";
+    import LayoutDefaultFooter from '../layout-default/layout-default-footer';
 
-        props: {
-            source: String,
+    export default {
+
+        components: {
+            AuthLogin,
+            AuthRegister,
+            LayoutDefaultFooter,
         },
 
-        data: () => ({
-            valid: false,
-            form: {
-                email: '',
-                password: '',
-                remember: false
-            },
-            passwordVisible: false,
-            rules: {
-                required: value => !!value || 'Field is required',
-                email: value => {
-                    const pattern = /.+@.+\..+/;
-                    return (
-                        pattern.test(value) ||
-                        'E-Mail must be valid.'
-                    );
-                },
-                length: value => {
-                    const minLength = 5;
-                    const maxLength = 255;
-                    return (
-                        (value.length >= minLength && value.length <= maxLength) ||
-                        'Field must be between 5 and 255 characters'
-                    );
-                }
-            },
-        }),
-
-        methods: {
-            submit() {
-                console.log('Submit')
-            },
+        data () {
+            return {
+                tab: null,
+            }
         },
     }
 </script>
+
+<style>
+    html{
+        overflow: hidden;
+    }
+    .authBackground{
+        background-image: url(https://source.unsplash.com/1280x720/daily?programming);
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+    }
+</style>
--- src/Frontend/Resources/app/modules/profile/page/profile-update/index.vue
+++ src/Frontend/Resources/app/modules/profile/page/profile-update/index.vue
@@ -1,11 +1,164 @@
 <template>
-    <div>
-        ProfileUpdate
-    </div>
+    <v-container>
+        <v-form
+            @submit.prevent="submit"
+            v-model="valid"
+        >
+            <v-text-field
+                id="username"
+                label="Username"
+                name="username"
+                v-model="form.username"
+                :rules="[rules.required]"
+                prepend-icon="far fa-user"
+                type="text"
+                selected
+                required
+            />
+
+            <v-row>
+                <v-col cols="6">
+                    <v-text-field
+                        id="firstname"
+                        label="Firstname"
+                        name="firstname"
+                        class="pl-8"
+                        v-model="form.firstname"
+                        :rules="[rules.required]"
+                        type="text"
+                        required
+                    />
+                </v-col>
+
+                <v-col cols="6">
+                    <v-text-field
+                        id="lastname"
+                        label="Lastname"
+                        name="lastname"
+                        v-model="form.lastname"
+                        :rules="[rules.required]"
+                        type="text"
+                        required
+                    />
+                </v-col>
+            </v-row>
+
+            <div class="py-3"></div>
+
+            <v-text-field
+                id="email"
+                label="E-Mail"
+                name="email"
+                v-model="form.email"
+                :rules="[rules.email, rules.length, rules.required]"
+                prepend-icon="far fa-envelope"
+                type="email"
+                required
+            />
+
+            <v-row>
+                <v-col cols="6">
+                    <v-text-field
+                        id="password"
+                        label="Password"
+                        name="password"
+                        v-model="form.password"
+                        :rules="[rules.length, rules.required]"
+                        prepend-icon="fas fa-key"
+                        :append-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                        @click:append="passwordVisible = !passwordVisible"
+                        :type="passwordVisible ? 'text' : 'password'"
+                        required
+                    />
+                </v-col>
+
+                <v-col cols="6">
+                    <v-text-field
+                        id="password_confirmation"
+                        label="Confirm Password"
+                        name="password_confirmation"
+                        v-model="form.passwordConfirmation"
+                        :rules="[rules.required]"
+                        @change="passwordConfirmation"
+                        :error-messages='passwordConfirmationError'
+                        type="password"
+                        required
+                    />
+                </v-col>
+            </v-row>
+
+            <v-divider class="py-3"/>
+
+            <v-btn
+                :disabled="!valid"
+                color="green"
+                type="submit"
+                block
+                rounded
+                large
+            >
+                <icon name="fas fa-sign-in-alt"/> &nbsp; Register
+            </v-btn>
+        </v-form>
+    </v-container>
 </template>
 
 <script>
     export default {
+        name: "auth-register",
 
+        data: () => ({
+            valid: false,
+            form: {
+                username: '',
+                firstname: '',
+                lastname: '',
+                email: '',
+                password: '',
+                passwordConfirmation: '',
+            },
+            passwordConfirmationError: '',
+            passwordVisible: false,
+            rules: {
+                required: value => !!value || 'Field is required',
+                email: value => {
+                    const pattern = /.+@.+\..+/;
+                    return (
+                        pattern.test(value) ||
+                        'E-Mail must be valid.'
+                    );
+                },
+                length: value => {
+                    const minLength = 8;
+                    const maxLength = 255;
+                    return (
+                        (value.length >= minLength && value.length <= maxLength) ||
+                        'Field must be between 8 and 255 characters'
+                    );
+                },
+            },
+        }),
+
+        methods: {
+            passwordConfirmation() {
+                if(this.form.password !== this.form.passwordConfirmation){
+                    this.passwordConfirmationError = "Passwords don't match!";
+                } else {
+                    this.passwordConfirmationError = "";
+                }
+            },
+
+            submit() {
+                axios.post(
+                    "api/v1/users",{
+                        username: this.form.username,
+                        firstname: this.form.firstname,
+                        lastname: this.form.lastname,
+                        email: this.form.email,
+                        password: this.form.password,
+                        password_confirmation: this.form.passwordConfirmation,
+                });
+            },
+        },
     }
 </script>
